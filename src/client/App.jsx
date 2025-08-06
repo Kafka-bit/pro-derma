2e016b2563850099cefbbcc0cd057fc0cd741838
Kafka-bit <salvatierradaniel287@gmail.com>
2025-08-05 20:02:24 -0600

    add some client routes

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,36 @@
-import './styles/index.css'
+import React from "react";
+import "./styles/index.css";
+import { Routes, Route } from "react-router-dom";
+
+//Componentes de prueba
+const LoginPage = () => {
+  <div>Página de inicio de sesión</div>;
+};
+const RegisterPage = () => {
+  <div>Página de Registro</div>;
+};
+const HomePage = () => {
+  <div>Página de inicio(Dashboard)</div>;
+};
+const PatientsPage = () => {
+  <div>Dashboard de Pacientes</div>;
+};
+const NotFoundPage = () => {
+  <div>404 - Página no encontrada</div>;
+};
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
-  )
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/patients" element={<PatientsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 }
-
-
 
 export default App;
